4628ee0059700f4a25d0d211d5775464db1fe4e7
Akanksha Kriti <akankshakriti09@gmail.com>
2023-05-27 20:44:55 +0530

    database phpmyadmin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'authdb',
+        'USER': 'root',
+        #'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
