fa82efe395b1a479bf01d4fd6835d49e235b4d1b
Akanksha Kriti <akankshakriti09@gmail.com>
2023-06-02 16:29:26 +0530

    views created and url path is added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from api.models import Profile, User
+from api.serializers import UserSerializer, MyTokenObtainPairSerializer, RegisterSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics, status
+from rest_framework.decorators import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
 # Create your views here.
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny)
+    serializer_class = RegisterSerializer
+
+@api_view(['GET' , 'POST'])
+@permission_classes([IsAuthenticated])
+def dashboard(request):
+    if request.method == "GET":
+        response = f"Hey {request.user}, You are seeing a GET response"
+        return Response({'response': response}, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        text = request.POST.get("text")
+        response = f"Hey {request.user}, Your tesxt is {text}"
+        return Response({"response": response}, status=status.HTTP_200_OK)
+    
+    return Response({'response': response}, status=status.HTTP_400_BAD_REQUEST)
