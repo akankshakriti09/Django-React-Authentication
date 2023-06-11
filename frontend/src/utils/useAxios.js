96cf43261ddc61ca823fc5666f5ff6ec31fcad0f
Akanksha Kriti <akankshakriti09@gmail.com>
2023-06-11 17:09:05 +0530

    cmtd

--- frontend/src/utils/useAxios.js
+++ frontend/src/utils/useAxios.js
@@ -3,3 +3,23 @@ import jwt_decode from "jwt-decode"
 import dayjs from "dayjs"
 import {useContext} from "react"
 import AuthContext from "../context/AuthContext"
+
+const baseURL = "http://127.0.0.1:8000/api"
+
+const useAxios = () => {
+    const { authTokens, setUser, setAuthTokens } = useContext(AuthContext)
+    
+    const axiosInstance = axios.create({
+        baseURL ,
+        headers : {Authorization: 'Bearer ${authTokens?.access}'}
+    })
+
+    axiosInstance.interceptors.request.use(async req => {
+        const user = jwt_decode(authTokens.access)
+        const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1
+
+        if (isExpired) return req
+
+        const response = await axios.post(``)
+    })
+}
