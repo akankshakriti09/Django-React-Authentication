31c531011bbd063c6770329167e680886dd2ecb8
Akanksha Kriti <akankshakriti09@gmail.com>
2023-06-12 14:26:49 +0530

    cmtd

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("token/", views.MyTokenObtainPairView.as_view()),
     path("token/refresh/", TokenRefreshView.as_view()),
     path("register/", views.RegisterView.as_view()),
-    path("dashboard/", views.dashboard)
+    path("test/", views.testEndPoint, name='test'),
+    path('', views.getRoutes),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -16,15 +16,26 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = ([AllowAny])
     serializer_class = RegisterSerializer
 
+# Get All Routes
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token/',
+        '/api/register/',
+        '/api/token/refresh/'
+    ]
+    return Response(routes)
+
+
 @api_view(['GET' , 'POST'])
 @permission_classes([IsAuthenticated])
-def dashboard(request):
-    if request.method == "GET":
-        response = f"Hey {request.user}, You are seeing a GET response"
-        return Response({'response': response}, status=status.HTTP_200_OK)
-    elif request.method == "POST":
-        text = request.POST.get("text")
-        response = f"Hey {request.user}, Your text is {text}"
-        return Response({"response": response}, status=status.HTTP_200_OK)
-    
-    return Response({'response': response}, status=status.HTTP_400_BAD_REQUEST)
+def testEndPoint(request):
+    if request.method == 'GET':
+        data = f"Congratulation {request.user}, your API just responded to GET request"
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        text = "Hello buddy"
+        data = f'Congratulation your API just responded to POST request with text: {text}'
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    return Response({}, status.HTTP_400_BAD_REQUEST)
