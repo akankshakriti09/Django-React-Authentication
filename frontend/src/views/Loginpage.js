d1b58e8399749e694fb63196dbd55a883f0535f5
Akanksha Kriti <akankshakriti09@gmail.com>
2023-06-19 17:49:44 +0530

    committed

--- frontend/src/views/Loginpage.js
+++ frontend/src/views/Loginpage.js
@@ -1,9 +1,131 @@
-import React from 'react';
+import React, { useContext } from "react";
+import bgImg from "../images/loginimg.jpg";
+import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 function Loginpage() {
+
+  const {loginUser} = useContext(AuthContext)
+
   return (
     <div>
-      
+      <>
+        <section className="vh-100">
+          <div className="container py-5 h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col col-xl-10">
+                <div className="card" style={{ borderRadius: "1rem" }}>
+                  <div className="row g-0">
+                    <div className="col-md-6 col-lg-5 d-none d-md-block">
+                      <img
+                        src={bgImg}
+                        alt="login form"
+                        className="img-fluid"
+                        style={{ borderRadius: "1rem 0 0 1rem" }}
+                      />
+                    </div>
+                    <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                      <div className="card-body p-4 p-lg-5 text-black">
+                        <form>
+                          <div className="d-flex align-items-center mb-3 pb-1">
+                            <i
+                              className="fas fa-cubes fa-2x me-3"
+                              style={{ color: "#ff6219" }}
+                            />
+                            <div className="d-flex align-items-center mb-3 pb-1">
+                              <i
+                                className="fas fa-cubes fa-2x me-3"
+                                style={{ color: "#ff6219" }}
+                              />
+                              <span className="h2 fw-bold mb-0">
+                                Welcome Here !!
+                              </span>
+                            </div>
+                          </div>
+                          <h5
+                            className="fw-normal mb-3 pb-3"
+                            style={{ letterSpacing: 1 }}
+                          >
+                            <b>Login to your account</b>
+                          </h5>
+                          <div className="form-outline mb-4">
+                            <input
+                              type="email"
+                              id="form2Example17"
+                              className="form-control form-control-lg"
+                              name="email"
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form2Example17"
+                            >
+                              Email address
+                            </label>
+                          </div>
+                          <div className="form-outline mb-4">
+                            <input
+                              type="password"
+                              id="form2Example27"
+                              className="form-control form-control-lg"
+                              name="password"
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form2Example27"
+                            >
+                              Password
+                            </label>
+                          </div>
+                          <div className="pt-1 mb-4">
+                            <button
+                              className="btn btn-dark btn-lg btn-block"
+                              type="submit"
+                            >
+                              Login
+                            </button>
+                          </div>
+                          <a className="small text-muted" href="#!">
+                            Forgot password?
+                          </a>
+                          <p
+                            className="mb-5 pb-lg-2"
+                            style={{ color: "#393f81" }}
+                          >
+                            Don't have an account?{" "}
+                            <Link to="/register" style={{ color: "#393f81" }}>
+                              Register Now
+                            </Link>
+                            {/* <Link to="/register" style={{ color: "#393f81" }}>
+                        Register Now 
+                      </Link> */}
+                          </p>
+                          <a href="#!" className="small text-muted">
+                            Terms of use.&nbsp;
+                          </a>
+                          <a href="#!" className="small text-muted">
+                            Privacy policy
+                          </a>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer className="bg-light text-center text-lg-start">
+          <div
+            className="text-center p-3"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          >
+            © 2020 - till date Copyright :&nbsp;
+            <a className="text-dark" href="https://mdbootstrap.com/">
+              Welcome.com
+            </a>
+          </div>
+        </footer>
+      </>
     </div>
   );
 }
--- frontend/src/views/Registerpage.js
+++ frontend/src/views/Registerpage.js
@@ -1,9 +1,124 @@
 import React from 'react';
+import bgImg from "../images/loginimg.jpg";
+import { Link } from 'react-router-dom'
 
 function Registerpage() {
   return (
     <div>
       
+      <>
+        <section className="vh-90">
+          <div className="container py-4 h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col col-xl-10">
+                <div className="card" style={{ borderRadius: "1rem" }}>
+                  
+                  <div className="row g-0">
+                    <div className="col-md-6 col-lg-5 d-none d-md-block">
+                      <img
+                        src={bgImg}
+                        alt="login form"
+                        className="img-fluid"
+                        style={{ borderRadius: "1rem 0 0 1rem" }}
+                      />
+                    </div>
+                    
+                    <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                       <div className="card-body p-4 p-lg-5 text-black">
+                        <form>
+                          <div className="d-flex align-items-center mb-3 pb-1">
+                            <i
+                              className="fas fa-cubes fa-2x me-3"
+                              style={{ color: "#ff6219" }}
+                            />
+                            <span className="h2 fw-bold mb-2">
+                              Welcome to this Website !!
+                            </span>
+                          </div>
+                          <h5
+                            className="fw-normal mb-2 pb-3"
+                            style={{ letterSpacing: 1 }}
+                          >
+                            <b>Sign Up</b>
+                          </h5>
+                          <div className="form-outline mb-4">
+                            <input
+                              type="email"
+                              id="form2Example17"
+                              className="form-control form-control-lg"
+                              placeholder="Email Address"
+                            />
+                          </div>
+                          <div className="form-outline mb-4">
+                            <input
+                              type="text"
+                              id="form2Example17"
+                              className="form-control form-control-lg"
+                              placeholder="Username"
+                            />
+                          </div>
+                          <div className="form-outline mb-4">
+                            <input
+                              type="password"
+                              id="form2Example17"
+                              className="form-control form-control-lg"
+                              placeholder="Password"
+
+                            />
+                          </div>
+                          <div className="form-outline mb-4">
+                            <input
+                              type="password"
+                              id="form2Example27"
+                              className="form-control form-control-lg"
+                              placeholder="Confirm Password"
+
+                            />
+                          </div>
+                          <div className="pt-1 mb-4">
+                            <button
+                              className="btn btn-dark btn-lg btn-block"
+                              type="submit"
+                            >
+                              Register
+                            </button>
+                          </div>
+                          <a className="small text-muted" href="#!">
+                            Forgot password?
+                          </a>
+                          <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
+                            Already have an account?{" "}
+                            <Link to="/login" style={{ color: "#393f81" }}>
+                              Login Here
+                            </Link>
+                          </p>
+                          <a href="#!" className="small text-muted">
+                            Terms of use.&nbsp;
+                          </a>
+                          <a href="#!" className="small text-muted">
+                            Privacy policy
+                          </a>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer className="bg-light text-center text-lg-start">
+          <div
+            className="text-center p-3"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          >
+            © 2020 - till date Copyright :&nbsp;
+            <a className="text-dark" href="https://mdbootstrap.com/">
+              Welcome.com
+              </a>
+          </div>
+        </footer>
+      </>
     </div>
   );
 }
